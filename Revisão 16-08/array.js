e661d29278aae02560d990950ffaadb8a95622ae
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2022-08-26 22:22:28 -0300

    Delete Revisão 16-08 directory

--- Revisão 16-08/array.js
+++
@@ -1,4 +0,0 @@
-let nomes = ['Mônica', 'Cebolinha', 'Cascão', 'Magali']
-nomes.forEach(nome => {
-    console.log(nome, nome.length)
-})
--- Revisão 16-08/revisão/while.js
+++
@@ -1,7 +0,0 @@
-
-isImpar = true
-while(isImpar){
-    let numero = Math.floor(Math.random() * 10000)
-    console.log(numero)
-    isImpar = numero % 2 == 1
-}
